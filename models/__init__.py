92035de606a382f9f7b20bb7365e855cf50f039a
Concerned Citizen <97725405+coffeeNtv@users.noreply.github.com>
2024-06-21 09:59:16 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .vihc import VIHC_model
-def create_model(opt):
-    model = None
-    model = VIHC_model()
-    model.initialize(opt)
-    print("model [%s] was created" % (model.name()))
-    return model
